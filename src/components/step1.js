9203fa7bc56db353ffc0998a15cd86b7fb6c5750
FJaku <jakubec.filip24@gmail.com>
2021-01-27 00:19:45 +0100

    Added transition to step2, finished donation amount selection

--- src/components/step1.js
+++ src/components/step1.js
@@ -7,7 +7,7 @@ import 'simplebar/dist/simplebar.min.css';
 
 const Step1 = () => {
     const dispatch = useDispatch();
-    const [selectedShelter, setSelectedShelter] = useState ('Vyberte si útulok zo zoznamu (nepovinné)') //Selected shelter  - ID or empty 
+    const [selectedShelter, setSelectedShelter] = useState ('Vyberte si útulok zo zoznamu') //Selected shelter  - ID or empty 
     const [selected, setSelected] = useState (['selected', 'notSelected']) //Class selection of donation type
 
     //Axios API call
@@ -27,14 +27,18 @@ const Step1 = () => {
             <p id="donationChoiceText">Vyberte si možnosť, ako chcete pomôcť </p>
             <div id="donationType">
                 <div id='wholeOrganization' className={selected[0]} onClick={() => {
-                            setSelected(['selected', 'notSelected'])
-                            setSelectedShelter('Vyberte si útulok zo zoznamu (nepovinné)') //List of shelters reset to default
+                            setSelected(['selected', 'notSelected']) //Swtich donation type
+                            setSelectedShelter('Vyberte si útulok zo zoznamu') //List of shelters reset to default
                             dispatch({ type: 'SHELTERCHOICE', payload: ''}) //Selected shelter reset to empty
                         }}>
                     <i className="fa fa-credit-card"></i>
                     <p>Chcem finančne prispieť celej nadácii</p>
                 </div>
-                <div id='selectedShelter' className={selected[1]} onClick={() => setSelected(['notSelected', 'selected'])}>
+                <div id='selectedShelter' className={selected[1]} onClick={() => 
+                            setSelected(['notSelected', 'selected'],
+                            dispatch({ type: 'SHELTERCHOICE', payload: 'required'}),
+                            setSelectedShelter('Vyberte si útulok zo zoznamu (povinné)')
+                        )}>
                     <i className="fa fa-paw"></i>
                     <p>Chcem finančne prispieť konkrétnemu útulku</p>
                 </div>
@@ -83,63 +87,100 @@ const Step1 = () => {
 
     //Donation change handler
     const handleDonationChange = (event) => {
-        dispatch({ type: 'LISTENDONATION', payload: event.target.value})
+        if (!isNaN(event.target.value)){
+            dispatch({ type: 'LISTENDONATION', payload: event.target.value}) //Update donation amount state
+        } else {
+            document.getElementById("customAmount").value =  document.getElementById("customAmount").value.replace(/\D/g,'') //Allow numbers only
+        }       
+    }
+
+    const clearPriorSelection = () => {
+        if (document.getElementsByClassName("selectedAmount")[0] !== undefined) { //Check if prior selection exists
+            document.getElementsByClassName("selectedAmount")[0].classList.remove('selectedAmount') //Clear prior selection
+        } 
     }
 
     //Donation amount
     const DonationAmount = () => {
         return (
-            <>
-                <p className="donationAmount, selectedAmount" id="10" onClick={() => {
+            <div id="donationAmount">
+                <p id="donationText">Suma, ktorou chcem prispieť</p>
+                <p className="donationAmount" id="10" onClick={() => {
                         dispatch({ type: 'LISTENDONATION', payload: '10'}) //Update state of donation amount
-                        document.getElementsByClassName("selectedAmount")[0].classList.remove('selectedAmount') //Clear any prior selection
+                        clearPriorSelection() //Clear any prior selection
                         document.getElementById('10').classList.add('selectedAmount') //Select clicked option
                     }}
                 >
-                    10
+                    10€
                 </p>
                 <p  className="donationAmount" id="25" onClick={() => {
                         dispatch({ type: 'LISTENDONATION', payload: '25'})
-                        document.getElementsByClassName('selectedAmount')[0].classList.remove('selectedAmount')
+                        clearPriorSelection()
                         document.getElementById('25').classList.add('selectedAmount')
                     }}
                 >
-                    25
+                    25€
                 </p>
                 <p  className="donationAmount" id="50" onClick={() => {
                         dispatch({ type: 'LISTENDONATION', payload: '50'})
-                        document.getElementsByClassName('selectedAmount')[0].classList.remove('selectedAmount')
+                        clearPriorSelection()
                         document.getElementById('50').classList.add('selectedAmount')
                     }}
                 >
-                    50
+                    50€
                 </p>
                 <form>
                     <input 
-                        id="customAmount"
-                        type='number'
-                        pattern='.{5}'
+                        id="customAmount"                        
+                        type='text'
+                        maxLength='5'
+                        placeholder="____€"
                         onChange={handleDonationChange}
                         onClick={() => {
-                            document.getElementsByClassName('selectedAmount')[0].classList.remove('selectedAmount')
+                            dispatch({ type: 'LISTENDONATION', payload: ''})
+                            clearPriorSelection()
                             document.getElementById('customAmount').classList.add('selectedAmount')
                         }}
                     />
                     <button type="submit" style={{display: 'none'}} onClick={(event) => event.preventDefault()}/> {/*Hidden button*/}
                 </form>
-                <p>Pokračovať</p>
-            </>
+            </div>
         )
     }
 
+    const step1 = useSelector(state => state.step1Reducer)
+
+    const firstTransition = () => {
+        if (store.getState().shelterChoiceReducer === 'required'){
+            var y = document.getElementById('displaySelectedShelter')
+            y.classList.add('highlight')
+            setTimeout(() => 
+                y.classList.remove('highlight')
+            ,500) 
+        } else if (!store.getState().donationReducer){ //Check if donation amount has been selected
+            var x = document.getElementById('donationText')
+            x.classList.add('highlight')
+            setTimeout(() => 
+                x.classList.remove('highlight')
+            ,500)
+        } else {
+            dispatch({ type: 'HIDESTEP1' }) //Proceed to next step
+            dispatch({ type: 'SHOWSTEP2' })
+        }
+        }   
+
     return (
-        <>
+        <div id="step1" className={step1}>
             <DonationType />
             <ShelterChoice key={useSelector(state => state.isLoadedReducer)} />
             <DonationAmount />
-            <p className="nextButton">Pokracovat</p>
-        </>      
+            <p 
+                className="nextButton"
+                id="step1Button"
+                onClick={firstTransition}
+            >Pokračovať</p>
+        </div>      
     )
-}
+} 
 
 export default Step1
