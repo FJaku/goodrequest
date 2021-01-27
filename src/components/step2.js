a5a2f009872c06996c8178e35a61d532f765f127
FJaku <jakubec.filip24@gmail.com>
2021-01-27 17:23:21 +0100

    Switched from HTML5 to JS form validation

--- src/components/step2.js
+++ src/components/step2.js
@@ -1,4 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react'
+import cz from '../img/cz.png'
+import svk from '../img/svk.png'
+import store from '../redux/store'
 
 const Step2 = () => {
     const dispatch = useDispatch();
@@ -13,70 +17,162 @@ const Step2 = () => {
         dispatch({ type: 'LISTENEMAIL', payload: event.target.value})
     }
     const handleNumberChange = (event) => {
-        dispatch({ type: 'LISTENNUMBER', payload: event.target.value})
-    }
-    const handleCountryChange = (event) => {
-        dispatch({ type: 'LISTENCOUNTRY', payload: event.target.value})
+        if (!isNaN(event.target.value)){
+            dispatch({ type: 'LISTENNUMBER', payload: event.target.value}) //Update number state
+        } else {
+            document.getElementById("numberInput").value =  document.getElementById("numberInput").value.replace(/\D/g,'') //Allow numbers only
+        }           
     }
 
-    const handleStep2 = (event) => {
-        event.preventDefault()
+    const step2 = useSelector(state => state.step2Reducer)
+    const [prefix, setPrefix] = useState({prefix: '+421', img:svk})
 
+    //Prefix switch
+    const chooseSVK = () => {
+        setPrefix({prefix: '+421', img:svk})
+        console.log(prefix.prefix)
+        dispatch({ type: 'LISTENCOUNTRY', payload: '+421'})
     }
+
+    const chooseCZ = () => {
+        setPrefix({prefix: '+420', img:cz})
+        dispatch({ type: 'LISTENCOUNTRY', payload: '+420'})
+    }
+
+    //Validity check + transition
+    const secondTransition = () => {
+        var firstName = store.getState().firstNameReducer
+        var lastName = store.getState().lastNameReducer
+        var email = store.getState().emailReducer
+        var number = store.getState().numberReducer
+        var valid = true
+
+        //First name validation
+        if (firstName.length === 0 || (20 >= firstName.length  && firstName.length >= 2)) {
+            console.log('First name OK')
+        } else {
+            valid = false
+            let x = document.getElementById('firstNameText')
+            x.classList.add('highlight')
+            setTimeout(() => 
+                x.classList.remove('highlight')
+            ,500)
+        }
+
+        //Last name validation
+        if (30 >= lastName.length  && lastName.length >= 2) {
+            console.log('Last name OK')
+        } else {
+            valid = false
+            let x = document.getElementById('lastNameText')
+            x.classList.add('highlight')
+            setTimeout(() => 
+                x.classList.remove('highlight')
+            ,500)
+        }
+
+        //Email validation
+        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+            console.log('Email OK')
+        } else {
+            valid = false
+            let x = document.getElementById('emailText')
+            x.classList.add('highlight')
+            setTimeout(() => 
+                x.classList.remove('highlight')
+            ,500)
+        }
+
+        //Number validation
+        if (number.length === 9) {
+            console.log('Number OK')
+        } else {
+            valid = false
+            let x = document.getElementById('numberText')
+            x.classList.add('highlight')
+            setTimeout(() => 
+                x.classList.remove('highlight')
+            ,500)
+        }
+
+        //Transition to step3
+        if (valid) {
+            dispatch({ type: 'HIDESTEP2' })
+            dispatch({ type: 'SHOWSTEP3' })
+        }
+    }
+
     return (
-        <>
+        <div id="step2" className={step2}>
+            <p className="stepMainText">Potrebujeme od Vás zopár informácií</p>
         <form>
             {/*First Name*/}
-            <input
-                pattern='.{0}|.{2,20}' //Length - empty or 2-20 char long
-                value={useSelector(state => state.firstNameReducer)}
-                onChange={handleFirstNameChange}
-                placeholder='First Name'
-            >
-            </input>
-            <br></br>
+            <div className="step2Input">
+                <p id="firstNameText">Meno</p>
+                <input
+                    type="text"
+                    value={useSelector(state => state.firstNameReducer)}
+                    onChange={handleFirstNameChange}
+                    placeholder='Zadajte vaše meno'
+                    id="fistNameInput"                   
+                >
+                </input>
+            </div>
             {/*Last Name*/}
-            <input
-                pattern='.{2,30}'
-                value={useSelector(state => state.lastNameReducer)}
-                onChange={handleLastNameChange}
-                placeholder='Last Name'
-                required
-            >
-            </input>
-            <br></br>
+            <div className="step2Input">
+                <p id="lastNameText">Priezvisko</p>
+                <input
+                    type="text"
+                    value={useSelector(state => state.lastNameReducer)}
+                    onChange={handleLastNameChange}
+                    placeholder='Zadajte vaše priezvisko'
+                    id="lastNameInput"
+                >
+                </input>
+            </div>
             {/*Email*/}
-            <input
-                type='email'
-                value={useSelector(state => state.emailReducer)}
-                onChange={handleEmailChange}
-                placeholder='Email'
-                required
-            >
-            </input>
-            <br></br>
-            {/*Country Prefix*/}
-            <select 
-                name='country'
-                onChange={handleCountryChange}
-            >
-                <option value='+421'>(+421)</option>
-                <option value='+420'>(+420)</option>
-            </select>
+            <div className="step2Input">
+                <p id="emailText">E-mailová adresa</p>
+                <input
+                    type='text'
+                    value={useSelector(state => state.emailReducer)}
+                    onChange={handleEmailChange}
+                    placeholder='Zadajte váš e-mail'
+                    id="emailInput"
+                >
+                </input>
+            </div>
+           
             {/*Number*/}
-            <input
-                type="number"
-                pattern='.{9}'
-                value={useSelector(state => state.numberReducer)}
-                onChange={handleNumberChange}
-                placeholder='Phone number'
-                required
-            >
-            </input>
-            <button type="submit" onClick={handleStep2}>Pokracovat</button> {/*Checks form validity, proceeds to step3*/}
+            <div className="step2Input">
+                <p id="numberText">Telefónne číslo</p>
+                 {/*Country Prefix*/}
+                <div id="prefixChoiceContainer">
+                    <p id="prefixText"><img src={prefix.img} alt=''></img><span>{prefix.prefix}</span></p>
+                    <ul id="prefixList">
+                        <li onClick={chooseSVK} id="svk" className="prefixListItem"><img src={svk} alt=''></img>+421</li>
+                        <li onClick={chooseCZ} id="cz" className="prefixListItem"><img src={cz} alt=''></img>+420</li>
+                    </ul>
+                </div>
+                <input
+                    type="text"
+                    maxLength="9"
+                    value={useSelector(state => state.numberReducer)}
+                    onChange={handleNumberChange}
+                    placeholder='Zadajte vaše číslo'
+                    id="numberInput"
+                >
+                </input>
+            </div>            
         </form>
         <p>Späť</p>
-        </>
+        <p 
+            className="nextButton" 
+            id="step2Button" 
+            onClick={secondTransition}
+            >Pokračovať
+        </p>
+        </div>
     )
 }
 
